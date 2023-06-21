13f5531040cdbf6c2704124a62bfafaad83e894b
Bobur112 <bahrommahmudov112@gmail.com>
2023-06-21 14:05:54 +0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,67 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import ocean from "./images/ocean.jpg";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div>
+      <header>
+        <div className="container">
+          <a href="#">Start Bootstrap</a>
+          <ul>
+            <li>
+              <a href="#">Home</a>
+            </li>
+            <li>
+              <a href="#">About</a>
+            </li>
+            <li>
+              <a href="#">Contact</a>
+            </li>
+          </ul>
+        </div>
       </header>
+      <section id="homeSection">
+        <div className="container">
+          <img src={ocean} width="150px" height="auto" />
+          <h1>Full Width Pics</h1>
+          <p>Landing Page Template</p>
+        </div>
+      </section>
+      <section id="text">
+        <div className="containerMini">
+          <h1>Full Width Backgrounds</h1>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Non
+            similique vero molestiae excepturi aut
+          </p>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas,
+            laboriosam.
+          </p>
+        </div>
+      </section>
+
+      <section className="imageSection imageSection1"></section>
+
+      <section id="text">
+        <div className="containerMini">
+          <h1>Full Width Backgrounds</h1>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Non
+            similique vero molestiae excepturi aut
+          </p>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas,
+            laboriosam.
+          </p>
+        </div>
+      </section>
+
+      <footer>
+        <div className="container">
+          <p>Copyright © Your Website 2023</p>
+        </div>
+      </footer>
     </div>
   );
 }
